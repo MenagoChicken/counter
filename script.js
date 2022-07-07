536b36cfcde4f854acc3d961ed43a0cc71ed8df8
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-07 21:23:37 +0100

    save&load&clear
    Save load clear logic

--- script.js
+++ script.js
@@ -17,6 +17,16 @@ buttons.forEach((button) => {
       case "reset":
         counter = 0;
         break;
+      case "save":
+        localStorage.setItem("counter", counter);
+        break;
+      case "load":
+        counter = localStorage.getItem("counter");
+        break;
+      case "clear":
+        localStorage.clear();
+        counter = 0;
+        break;
     }
     document.querySelector(".counter-value").innerText = counter;
     console.log(document.querySelector(".counter-value").innerText);
