6c06630b53e204a616ceef2e7188301b79a60b27
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-07 21:04:44 +0100

    JS-working-buttons
    Buttons console log show proper value

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello world!");
+const buttons = Array.from(document.querySelectorAll(".btn"));
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const type = button.getAttribute("id");
+    console.log(type);
+  });
+});
