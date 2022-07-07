9582bc790f908ed85e6e2e0c0ac2f77b953384d9
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-07 21:13:54 +0100

    + & -
    Increasing & decreasing & reset

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const buttons = Array.from(document.querySelectorAll(".btn"));
 
+let counter = 0;
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const type = button.getAttribute("id");
     console.log(type);
+
+    switch (type) {
+      case "increase":
+        counter++;
+        break;
+      case "decrease":
+        counter--;
+        break;
+      case "reset":
+        counter = 0;
+        break;
+    }
+    document.querySelector(".counter-value").innerText = counter;
+    console.log(document.querySelector(".counter-value").innerText);
   });
 });
